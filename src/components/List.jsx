830b0d3b02b0c189d000dbf6bd5ae4ebf4c84bfd
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-09-23 00:17:42 +0600

    ada

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,7 +4,7 @@ import Items from "./Items";
 
 const List = ({ users, errors, fetchUser }) => {
   return (
-    <div className="p-4 md:sticky top-24 mt-12 static max-j-[800px]   bg-white rounded-xl  shadow-md">
+    <div className="p-4 md:sticky top-24 mt-12 static max-j-[800px]   bg-white rounded-3xl  shadow-md">
       <h1 className="text-xl font-bold mb-4">Added Users</h1>
       <ul className="my-3 text-hero-gray  ">
         {users.length <= 0 && <li className="  mt-2  ">No Users Found!</li>}
