96c2d7ded7cc01b57910bd5bb6731b98e93adac0
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-09-22 20:30:23 +0600

    a

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -6,7 +6,7 @@ const List = ({ users, errors, fetchUser }) => {
   return (
     <div className="p-4 md:sticky top-24 mt-12 static max-j-[800px]   bg-white rounded-xl  shadow-md">
       <h1 className="text-xl font-bold mb-4">Added Users</h1>
-      <ul className="my-3 text-hero-gray  overflow-y-scroll">
+      <ul className="my-3 text-hero-gray  ">
         {users.length <= 0 && <li className="  mt-2  ">No Users Found!</li>}
         {errors && <li className="  mt-2  ">{errors}</li>}
 
