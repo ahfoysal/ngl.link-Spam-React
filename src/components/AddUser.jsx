1007d0b6ae9eae3dc95181bff12708577fbddca3
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-09-23 15:04:02 +0600

    a

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import supabase from "../config/supbase";
 import { toast } from "react-toastify";
 import { encodePassword, extractUserName } from "../utils/helper";
+import axios from "axios";
 
 // eslint-disable-next-line react/prop-types
 const AddUser = ({ fetchUser }) => {
   const [userName, setUserName] = useState("");
   const [customMessage, setCustomMessage] = useState("");
+  const [data, setData] = useState([]);
   const [key, setKey] = useState("");
   const [showCustomMessageButton, setShowCustomMessageButton] = useState(true);
   const [showPasswordButton, setShowPasswordButton] = useState(true);
@@ -50,6 +52,33 @@ const AddUser = ({ fetchUser }) => {
     setCustomMessage("");
     setKey("");
   };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Replace 'YOUR_TOKEN' with your actual authorization token
+        const config = {
+          headers: {
+            Authorization: import.meta.env.VITE_token,
+          },
+        };
+
+        const response = await axios.get(import.meta.env.VITE_url, config);
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // Fetch data initially
+    fetchData();
+
+    // Set up an interval to fetch data every 5 seconds (adjust as needed)
+    const intervalId = setInterval(fetchData, 5000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className="">
@@ -111,6 +140,11 @@ const AddUser = ({ fetchUser }) => {
           Add!
         </button>
       )}
+      <div className="mt-10">
+        <h2 className="text-[#FBB4A9] font-semibold text-lg">
+          Total Message Sent
+        </h2>
+      </div>
     </div>
   );
 };
